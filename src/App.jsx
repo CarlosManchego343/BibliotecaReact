5b2093c29f9edbbc6e695f86e2548837e5ede156
Wendigo43 <carlosmariomanchego1@gmail.com>
2023-10-27 17:29:21 -0500

    Feature-how put images-CMMP

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Component } from './component'
-import * as API from "./services/data";
-
+import { getCharacters } from "./services/data";
+import logo from "./assets/img/logo.png";
 
 // Este es el componente principal, se exporta para ser usado
 // en el archivo main
@@ -9,22 +9,44 @@ import * as API from "./services/data";
 export function App() {
 
   const [characters, setCharacters] = useState([])
+  const [isLoading, setIsLoading] = useState(true); 
 
   // Se va a usar el hook de efecto para hacer la petición de los
   // datos a la API y luego se almacena dentro del componenete 
   // "Characters" por medio del setCharacters.
   useEffect(() => {
-    API.getCharacters().then(setCharacters);
-  })
+
+    // Se hace un metodo asincorono para esperar a llegada de datos
+    // esto con el fin de para los casos donde no hayan datos
+
+    const fetchData = async () => {
+      try {
+        const data = await getCharacters();
+        setCharacters(data.data.results);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
 // Se engloba todo en "<></>" para que no haya cproblemas con el
 // renderizado de los estilos de los elementos dentto
     <>
-    <h2>Personajes del mundo marvel</h2>
-      {
-        JSON.stringify(characters)
-      }
+    <h2>Personajes del mundo</h2>
+    <img src={logo} height='50px'></img>
+      {isLoading ? (
+        <p>Cargando...</p>
+      ) : (
+        <ul>
+          {characters.map((character) => (
+            <li key={character.id}>{character.name}</li>
+          ))}
+        </ul>
+      )}
     </>
   )
 }
--- src/services/data.js
+++ src/services/data.js
@@ -1,18 +1,18 @@
-const URL = "https://gateway.marvel.com:443/v1/public/series?"
+const URL = "https://gateway.marvel.com:443/v1/public/characters?"
 
-    const public_key = "3020e533d2787d68dc61d8362a92516d"
+const public_key = "3020e533d2787d68dc61d8362a92516d"
 // private_key = 8fff12f9385ad8e7e745a0fa5e09784e98ca596c
-    const time_stap = "1"
+const time_stap = "1"
 // 18fff12f9385ad8e7e745a0fa5e09784e98ca596c3020e533d2787d68dc61d8362a92516d
-    const hash = "ae84b281d404769ed93abf500596ec59";
+const hash = "ae84b281d404769ed93abf500596ec59";
 
-export function getCharacters() {
+export async function getCharacters() {
     try {
-        return fetch(URL + "ts=" + time_stap + "&apikey=" + public_key + "&hash=" + hash).then(response => {
-            let data = response.json();
-            console.log("Entre a data")
-            return data;
-        })
+        // Se hace un metodo asincorono para esperar a llegada de datos
+        // esto con el fin de para los casos donde no hayan datos
+        const response = await fetch(URL + "ts=" + time_stap + "&apikey=" + public_key + "&hash=" + hash);
+        const data = await response.json();
+        return data;
     } catch (error) {
         console.error(error);
     }
